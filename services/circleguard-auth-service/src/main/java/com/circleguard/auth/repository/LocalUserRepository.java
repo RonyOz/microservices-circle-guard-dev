00e571854c6de60bd3ec6f948eacf6704f96f7a8
e5jparra1 <e5jparra@gmail.com>
2026-05-26 20:30:53 -0500

    fix(auth-service): add findUsersByPermissionName query to LocalUserRepository

--- services/circleguard-auth-service/src/main/java/com/circleguard/auth/repository/LocalUserRepository.java
+++ services/circleguard-auth-service/src/main/java/com/circleguard/auth/repository/LocalUserRepository.java
@@ -2,9 +2,15 @@ package com.circleguard.auth.repository;
 
 import com.circleguard.auth.model.LocalUser;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import java.util.List;
 import java.util.UUID;
 import java.util.Optional;
 
 public interface LocalUserRepository extends JpaRepository<LocalUser, UUID> {
     Optional<LocalUser> findByUsername(String username);
+
+    @Query("SELECT u FROM LocalUser u JOIN u.roles r JOIN r.permissions p WHERE p.name = :permissionName")
+    List<LocalUser> findUsersByPermissionName(@Param("permissionName") String permissionName);
 }
